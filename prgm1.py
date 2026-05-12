a00a90efc6aca339c201446b121c6c2b1de597a1
ABBU AK <143691280+mrakabu@users.noreply.github.com>
2026-05-12 10:30:22 +0530

    Implement data visualization with histograms and box plots
    Added data visualization using histograms and box plots.

--- prgm1.py
+++ prgm1.py
@@ -1,9 +1,25 @@
+#prg 1...
+# Import libraries
+import pandas as pd
 import matplotlib.pyplot as plt
 
-x=[24,56,86,53]
-y=[1,2,3,4]
-plt.plot(x,y)
-plt.xlabel("")
-plt.ylabel("")
-plt.title("")
+# Sample dataset
+data = {
+    'Age': [20, 22, 21, 23, 24, 25, 26, 27, 28, 29],
+    'Height': [150, 155, 160, 165, 170, 175, 180, 185, 190, 195],
+    'Weight': [45, 48, 50, 55, 60, 65, 70, 75, 80, 85]
+}
+
+df = pd.DataFrame(data)
+
+
+df.hist(figsize=(10, 6), bins=5)
+
+plt.suptitle("Histogram for Numerical Features")
+plt.show()
+
+
+df.plot(kind='box', figsize=(8, 5))
+
+plt.title("Box Plot for Numerical Features")
 plt.show()
