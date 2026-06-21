17ff6f712ec89c9b3e65f7c06d53230cbbf28547
ABBU AK <143691280+mrakabu@users.noreply.github.com>
2026-06-21 13:12:53 +0530

    Revise program to analyze California Housing dataset
    Updated the program to analyze the California Housing dataset by generating histograms and box plots for numerical features, while also including data validation checks.

--- prgm1.py
+++ prgm1.py
@@ -1,25 +1,31 @@
 #prg 1...
 # Import libraries
-import pandas as pd
+#1.Develop a program to create histograms for all numerical features and analyze the distribution of each feature. Generate box plots for all numerical features and identify any outliers. Use California Housing
+#dataset.
+import numpy as np
 import matplotlib.pyplot as plt
-
-# Sample dataset
-data = {
-    'Age': [20, 22, 21, 23, 24, 25, 26, 27, 28, 29],
-    'Height': [150, 155, 160, 165, 170, 175, 180, 185, 190, 195],
-    'Weight': [45, 48, 50, 55, 60, 65, 70, 75, 80, 85]
-}
-
-df = pd.DataFrame(data)
-
-
-df.hist(figsize=(10, 6), bins=5)
-
-plt.suptitle("Histogram for Numerical Features")
+import pandas as pd
+import seaborn as sns
+df = pd.read_csv(r"D:\ML\Datasets\housing.csv")
+print(df.head(10))
+print(df.shape)
+df.info()
+df.nunique()
+print(df.nunique())
+df.isnull().sum()
+print(df.isnull().sum())
+df.duplicated().sum()
+print(df.duplicated().sum())
+df.describe()
+print(df.describe())
+Numerical = df.select_dtypes(include=[np.number]).columns 
+print(Numerical)
+for col in Numerical: plt.figure(figsize=(10, 6))
+df[col].plot(kind='hist', title=col, bins=60, edgecolor='black') 
+plt.ylabel('Frequency' )
 plt.show()
-
-
-df.plot(kind='box', figsize=(8, 5))
-
-plt.title("Box Plot for Numerical Features")
+for col in Numerical: plt.figure(figsize=(6, 6)) 
+sns.boxplot(df[col], color='blue')
+plt.title(col) 
+plt.ylabel(col)
 plt.show()
